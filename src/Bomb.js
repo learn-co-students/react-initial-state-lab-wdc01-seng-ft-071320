396a20de2eff3db5f6baad94baffbe42bedb947a
JinaStrickland <Jina30@gmail.com>
2020-09-14 23:32:08 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,44 @@
-// your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    render () {
+        return (
+            <div>
+                { this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!` }
+            </div>
+        )
+    }
+}
+
+
+// class currentSlideIndex extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//     }
+
+//     increment = () => {
+//         const newCount = this.state.count + 1
+//         this.setState({
+//             count: newCount 
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div >
+//                 { this.state.count }
+//             </div>
+//         )
+//     }
+// }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,42 @@
-// your ImageSlider code here!
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            <h1>
+                I am on slide { this.state.currentSlideIndex }
+            </h1>
+        )
+    }
+}
+
+// class currentSlideIndex extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//     }
+
+//     increment = () => {
+//         const newCount = this.state.count + 1
+//         this.setState({
+//             count: newCount 
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div >
+//                 { this.state.count }
+//             </div>
+//         )
+//     }
+// }
